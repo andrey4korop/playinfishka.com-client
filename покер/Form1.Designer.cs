5172ae837911125bc1494381fbfd2b4bab9e04c7
andrey4korop <andrey999@i.ua>
2016-12-20 22:59:48 +0200

    Latest ver
    In this read last 15 games and than sends this

--- покер/Form1.Designer.cs
+++ покер/Form1.Designer.cs
@@ -28,45 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
-            this.lbWindows = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 26);
+            this.button1.Location = new System.Drawing.Point(112, 107);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Let\'s GO!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // lbWindows
+            // textBox1
             // 
-            this.lbWindows.FormattingEnabled = true;
-            this.lbWindows.Location = new System.Drawing.Point(12, 77);
-            this.lbWindows.Name = "lbWindows";
-            this.lbWindows.Size = new System.Drawing.Size(260, 160);
-            this.lbWindows.TabIndex = 1;
+            this.textBox1.Location = new System.Drawing.Point(42, 183);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 292);
+            this.textBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.dsdsd;
+            this.pictureBox1.Location = new System.Drawing.Point(487, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 339);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.lbWindows);
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.Text_black_wallpaper_backgrounds;
+            this.ClientSize = new System.Drawing.Size(586, 549);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "PLAY in FISHKA.COM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox lbWindows;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- покер/Form1.cs
+++ покер/Form1.cs
@@ -4,15 +4,62 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.IO;
 
 namespace покер
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, 
+                                                long cButtons, long dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        private static string POST(string Url, string Data)
+        {
+            try
+            {
+                System.Net.WebRequest req = System.Net.WebRequest.Create(Url);
+                req.Method = "POST";
+                req.Timeout = 100000;
+                req.ContentType = "application/x-www-form-urlencoded";
+                byte[] sentData = Encoding.GetEncoding("utf-8").GetBytes(Data);
+                req.ContentLength = sentData.Length;
+                System.IO.Stream sendStream = req.GetRequestStream();
+                sendStream.Write(sentData, 0, sentData.Length);
+                sendStream.Close();
+                System.Net.WebResponse res = req.GetResponse();
+                System.IO.Stream ReceiveStream = res.GetResponseStream();
+                System.IO.StreamReader sr = new System.IO.StreamReader(ReceiveStream, Encoding.UTF8);
+                //Кодировка указывается в зависимости от кодировки ответа сервера
+                Char[] read = new Char[256];
+                int count = sr.Read(read, 0, 256);
+                string Out = String.Empty;
+                while (count > 0)
+                {
+                    String str = new String(read, 0, count);
+                    Out += str;
+                    count = sr.Read(read, 0, 256);
+                }
+                return Out;
+            }
+            catch
+            {
+                return "";
+                MessageBox.Show("хуйня");
+            }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
@@ -20,128 +67,152 @@ namespace покер
 
         private void button1_Click(object sender, EventArgs e)
         {
-            IntPtr ptr = WinAPI.FindWindow(null, "Текущая история раздач");
 
+         /*   System.Threading.Thread.Sleep(5000);
+            int x = this.Left;
+            int y = this.Top;
+            Cursor.Position = new Point(x + 620, y + 299);
+
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP,
+                        x, y, 0, 0);
+            System.Threading.Thread.Sleep(1000);
             
-
-            //Если окно найдено, то отбращаемся к его дочерним объектам
-
-            if (ptr.ToInt32() != 0)
-
-            {
-
-                IntPtr[] child = new IntPtr[10];
-                IntPtr[] child2 = new IntPtr[5];
-
-                child[0] = WinAPI.GetWindow(ptr, WinAPI.GetWindow_Cmd.GW_CHILD);
-
-                StringBuilder title = new StringBuilder(2000);
-
-
-                for (int i = 1; i <= 3; i++)
-
-                {
-                    
-                    child[i] = WinAPI.GetWindow(child[i - 1], WinAPI.GetWindow_Cmd.GW_HWNDNEXT);
-                    WinAPI.SendMessage(child[i - 1], Convert.ToInt32(WinAPI.GetWindow_Cmd.WM_GETTEXT), (IntPtr)20, title);
-                    lbWindows.Items.Add(title.ToString());
-                    
-                    if (i == 3)
-                    {
-                        lbWindows.Items.Add("от здесь");
-                        child2[0] = WinAPI.GetWindow(child[i-1], WinAPI.GetWindow_Cmd.GW_CHILD);
-                        int d= WinAPI.GetWindowTextLength(child2[0]);
-                        lbWindows.Items.Add(d.ToString());
-                   
-                        WinAPI.SendMessage(child2[0], Convert.ToInt32(WinAPI.GetWindow_Cmd.WM_GETTEXT), (IntPtr)26, title);
-                        lbWindows.Items.Add(title.ToString());
-                        WinAPI.GetWindowText(child2[0], title, 20);
-                        lbWindows.Items.Add(title.ToString());
-                        
-
-
-
-                    }
-                    
-
-                    
-
-                }
+            Cursor.Position = new Point(x + 620, y + 350);
+            System.Threading.Thread.Sleep(1000);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP,
+                        x, y, 0, 0);
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("^a");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("^c");
+            System.Threading.Thread.Sleep(100);
+            textBox1.Text = Clipboard.GetText();
+            string qww = "log=" + textBox1.Text;
+            //label2.Text = qww;*/
+            //POST("http://poker.loc/object.php", "log=rabotaet");
+            //POST("http://playinfishka.com/object.php", qww);
 
 
 
 
-                            
 
 
-            }
-
-
+            timer1.Enabled = true;
+           
+           // const int x = 32000;
+           // const int y = 32000;
+           // mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP,
+                      //  x, y, 0, 0);
         }
-    }
 
-    public static class WinAPI
-
-    {
-
-        ///  <summary>
-
-        ///  Найти окно
-
-        ///  </summary>
-
-        ///  <param name="lpClassName">Имя класса окна</param>
-
-        ///  <param name="lpWindowName">Имя окна</param>
-
-        ///  <returns></returns>
-
-        [DllImport("user32.dll", SetLastError = true)]
-
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-
-
-        [DllImport("user32.dll", SetLastError = true)]
-
-        public static extern IntPtr GetWindow(IntPtr HWnd, GetWindow_Cmd cmd);
-
-
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-
-        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, [Out]  StringBuilder lParam);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern int GetWindowText(IntPtr hWnd, [Out] StringBuilder lpString, int nMaxCount);
-
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        public static extern int GetWindowTextLength(IntPtr hWnd);
-
-
-        public enum GetWindow_Cmd : uint
-
+        private void Form1_Load(object sender, EventArgs e)
         {
 
-            GW_HWNDFIRST = 0,
+        }
+        int i;
+        int x;
+        int y;
+        public string[] send  = new string[30];
+        string qww;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+             x = Left;
+             y = Top;
+            textBox1.Text = "";
+            Cursor.Position = new Point(x + 620, y + 299);
 
-            GW_HWNDLAST = 1,
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP,
+                        x, y, 0, 0);
+            System.Threading.Thread.Sleep(50);
+            for (i = 1; i <= 15; i++)
+            {
+                SendKeys.Send("{UP}");
+                System.Threading.Thread.Sleep(5);
+            }
+            System.Threading.Thread.Sleep(500);
+            for (i = 1; i <= 20; i++)
+            {
+                Cursor.Position = new Point(x + 620, y + 299);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+                System.Threading.Thread.Sleep(50);
+                SendKeys.Send("{DOWN}");
+                Cursor.Position = new Point(x + 620, y + 350);
+                System.Threading.Thread.Sleep(100);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP,
+                            x, y, 0, 0);
+                System.Threading.Thread.Sleep(100);
+                SendKeys.Send("^a");
+                System.Threading.Thread.Sleep(100);
+                SendKeys.Send("^c");
+                System.Threading.Thread.Sleep(100);
+                this.send[i] = Clipboard.GetText();
+                
+            }
+            if ((send[15] == send[16]) && (send[16] == send[17]) && (send[17] == send[18]) && (send[18] == send[19]) && (send[19] == send[20]))
+            {
+                for (i = 1; i <= 20; i++)
+                {
+                    qww = "log=" + send[i];
+                    textBox1.Text += qww;
+                    POST("http://playinfishka.com/object.php", qww);
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            else
+            {
+                Cursor.Position = new Point(x + 620, y + 299);
 
-            GW_HWNDNEXT = 2,
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP,
+                            x, y, 0, 0);
+                System.Threading.Thread.Sleep(50);
+                for (i = 1; i <= 15; i++)
+                {
+                    SendKeys.Send("{UP}");
+                    System.Threading.Thread.Sleep(5);
+                }
+                System.Threading.Thread.Sleep(500);
+                for (i = 1; i <= 20; i++)
+                {
+                    Cursor.Position = new Point(x + 620, y + 299);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+                    System.Threading.Thread.Sleep(50);
+                    SendKeys.Send("{DOWN}");
+                    Cursor.Position = new Point(x + 620, y + 350);
+                    System.Threading.Thread.Sleep(100);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP,
+                                x, y, 0, 0);
+                    System.Threading.Thread.Sleep(100);
+                    SendKeys.Send("^a");
+                    System.Threading.Thread.Sleep(100);
+                    SendKeys.Send("^c");
+                    System.Threading.Thread.Sleep(100);
+                    send[i] = Clipboard.GetText();
+                    
+                }
+                textBox1.Text = "";
+                for (i = 1; i <= 20; i++)
+                {
+                    qww = "log=" + send[i];
+                    textBox1.Text += qww;
+                    POST("http://playinfishka.com/object.php", qww);
+                    System.Threading.Thread.Sleep(100);
+                }
 
-            GW_HWNDPREV = 3,
+            }
+            //textBox1.Text = Clipboard.GetText();
+            
+            //label2.Text = qww;
+            //POST("http://poker.loc/object.php", "log=rabotaet");
+            //POST("http://playinfishka.com/object.php", qww);
 
-            GW_OWNER = 4,
 
-            GW_CHILD = 5,
 
-            GW_ENABLEDPOPUP = 6,
-
-            WM_GETTEXT = 0x000D
 
         }
 
-
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
     }
-
 }
